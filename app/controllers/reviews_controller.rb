a7bdc9129727c7d683dea222f9d6c5e239f9139d
EmanuelN <emanuelneves10@gmail.com>
2017-09-01 16:21:11 -0400

    added form logic and alert

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    @review.user = current_user
+    if @review.save
+      redirect_to :root
+    else
+      redirect_to @product, :flash => {:error => "Must be logged in to review."}
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :product_id, :description)
+  end
+end
