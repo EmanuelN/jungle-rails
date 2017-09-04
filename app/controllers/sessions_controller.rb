9da3bb201501ceec558d3869502dd9e8106ba088
EmanuelN <emanuelneves10@gmail.com>
2017-09-04 14:22:48 -0400

    logs in properly

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
   def create
     if user = User.authenticate_with_credentials(params[:email], params[:password])
-      session[:user_id] = user
+      session[:user_id] = user.id
       redirect_to '/'
     else
       @error = "Incorrect email and/or password"
