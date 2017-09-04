00d340ce7f5a970c984261d289d79cf6eac965a0
EmanuelN <emanuelneves10@gmail.com>
2017-09-04 12:33:41 -0400

    added user_spec, password and email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,16 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reviews
 
+
+
+  validates :password_confirmation, presence: true
+  validates :password, length: { minimum: 6 }
   validates :first_name, presence: true
   validates :last_name, presence:true
+
+  validates :email, confirmation: true
   validates :email, presence:true
+  validates :email, uniqueness: { case_sensitive: false }
   validates_format_of :email, :with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  context "Validations:" do
+    before do
+      @user = User.create(
+        first_name: "Emanuel",
+        last_name: "Neves",
+        password: "password",
+        password_confirmation: "password",
+        email: "test@test.com"
+        )
+    end
+
+    it { is_expected.to validate_presence_of :first_name }
+    it { is_expected.to validate_presence_of :last_name }
+    it { is_expected.to validate_presence_of :email }
+    it { is_expected.to validate_presence_of :password }
+    it { is_expected.to validate_presence_of :password_confirmation }
+    it { is_expected.to validate_confirmation_of :password}
+
+    context "failures:" do
+      before do
+        @user.password_confirmation="shouldntwork"
+        @user.save
+        @user2 = User.create(
+          first_name: "Bob",
+          last_name: "Nahman",
+          password: "123",
+          password_confirmation: "123",
+          email: "test@test.COM"
+          )
+      end
+
+      it "doesn't save if passwords don't match" do
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it "doesn't save if email is same as other user" do
+        expect(@user2.errors.full_messages).to include("Email has already been taken")
+      end
+
+      it "doesn't save if password is too weak" do
+        expect(@user2.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      end
+
+    end
+  end
+end
