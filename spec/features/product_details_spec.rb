2e824658834a99739f9ac39a55a846f4638f8e11
EmanuelN <emanuelneves10@gmail.com>
2017-09-05 13:27:21 -0400

    feature test product details

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "ProductDetails", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price_cents: 6499
+        )
+    end
+  end
+
+  scenario "They see a product's details" do
+    # ACT
+    visit root_path
+    all('.product').first.click_on "Details"
+
+    # DEBUG / VERIFY
+    expect(page).to have_css '.product-detail'
+    expect(page).to have_content @category.products.last.name
+    save_screenshot
+
+  end
+
 end
