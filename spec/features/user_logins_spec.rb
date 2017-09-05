eba43be447fd4f7e118b784738a485704ac999bd
EmanuelN <emanuelneves10@gmail.com>
2017-09-05 14:45:16 -0400

    user login test spec

---
+++ spec/features/user_logins_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "UserLogins", type: :feature, js: true do
+  before :each do
+    @user = User.create(
+      first_name: "Test",
+      last_name: "McTestor",
+      password: "test12345",
+      password_confirmation: "test12345",
+      email: "test@mctestor.com"
+      )
+  end
+
+  scenario "User logging in" do
+    # ACT
+    visit root_path
+    click_on "Login"
+
+    fill_in "email", with: "test@mctestor.com"
+    fill_in "password", with: "test12345"
+    save_screenshot
+    click_on "Submit"
+
+    # DEBUG / VERIFY
+
+    expect(page).to have_content "Test"
+    save_screenshot
+  end
+
+end
